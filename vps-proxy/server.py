7b8f42dd4efeb12b66b63944c8f80b2d29a41d26
wang zhiyi <wzy164310431@gmail.com>
2026-04-23 19:41:24 +0800

    Allow all CORS request headers in VPS proxy

--- vps-proxy/server.py
+++ vps-proxy/server.py
@@ -27,7 +27,7 @@ app.add_middleware(
     allow_origins=list(parse_origins(ALLOWED_ORIGINS)),
     allow_credentials=False,
     allow_methods=["POST", "GET", "OPTIONS"],
-    allow_headers=["Content-Type", "Authorization"],
+    allow_headers=["*"],
 )
 
 
